71ecdcb8ad3d290ab17419eb57b21c86a185bfa8
Thomas <thomascoxpowell@gmail.com>
2025-08-08 23:43:36 -0400

    feat: add registration handler

--- api/route_handlers.go
+++ api/route_handlers.go
@@ -3,17 +3,51 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/thomascpowell/drive/jobs"
+	"github.com/thomascpowell/drive/models"
+	"github.com/thomascpowell/drive/utils"
+	"net/http"
+	"strings"
 )
 
 func handleAuth(dispatcher *jobs.Dispatcher) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-
 	}
 }
 
 func handleRegister(dispatcher *jobs.Dispatcher) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-
+		var creds models.Credentials
+		if err := ctx.ShouldBindJSON(&creds); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
+			return
+		}
+		if strings.TrimSpace(creds.Username) == "" || strings.TrimSpace(creds.Password) == "" {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "username and password are required"})
+			return
+		}
+		hashedPassword, err := utils.HashPassword(creds.Password)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		user := models.User{
+			Username: creds.Username,
+			Password: hashedPassword,
+		}
+		job := &models.Job{
+			ID:      utils.UUID(),
+			Type:    models.RegisterUser,
+			Payload: &user,
+			Done:    make(chan models.Result, 1),
+		}
+		dispatcher.Dispatch(job)
+		defer close(job.Done)
+		result := <-job.Done
+		if result.Err != nil { // check for username collision?
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": result.Err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{"message": "user registered"})
 	}
 }
 
