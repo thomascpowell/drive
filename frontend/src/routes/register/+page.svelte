3a851bf5b4b7e01975d220d8ab2d219c8e54a6dc
Thomas <thomascoxpowell@gmail.com>
2025-08-18 10:54:25 -0400

    feat(frontend): add basic register form

--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
   import { register } from "$lib/register";
+  import type { Res } from "$lib/types"
 
   let username = "";
   let password = "";
-  let res; //TODO: Use this w some sort of status component
+  let res:Res; //TODO: Use this w some sort of status component
 
   async function handleSubmit(e: Event) {
     e.preventDefault();
-    res = register(username, password);
+    res = await register(username, password);
+    console.log(res)
   }
 </script>
 
 <p>register</p>
 
 <form on:submit|preventDefault={handleSubmit} action="submit">
-  <input type="text" placeholder="username" />
-  <input type="text" placeholder="password" />
+  <input type="text" bind:value={username} placeholder="username" />
+  <input type="text" bind:value={password} placeholder="password" />
   <button>submit</button>
 </form>
