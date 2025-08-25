14eb46b88b88d15c26ad8d18ed8bd08101f6a632
Thomas <thomascoxpowell@gmail.com>
2025-08-24 22:42:12 -0400

    feat(frontend): add links in /files

--- frontend/src/routes/files/+page.svelte
+++ frontend/src/routes/files/+page.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { get_files } from "$lib/get_files"
-  import type { File } from "$lib/types"
+  import { get_files } from "$lib/get_files";
+  import type { File } from "$lib/types";
 
-  let files: File[]
+  let files: File[];
   onMount(async () => {
     files = await get_files();
-    console.log(files)
+    console.log(files);
   });
 </script>
 
-{#each files as file}
-  <p>{file.Filename}</p>
-{/each}
+<div>
+  {#each files as file}
+    <a href={"/files/" + file.ID}>{file.Filename}</a>
+  {/each}
+</div>
+
+<style>
+  div {
+    display: flex;
+    gap: 1em;
+    flex-direction: column;
+  }
+</style>
