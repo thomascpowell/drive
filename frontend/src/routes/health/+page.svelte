88cbe7682d46c8a7acd3c73489415ed489de85dc
Thomas <thomascoxpowell@gmail.com>
2025-08-26 11:03:34 -0400

    feat: add basic styles

---
+++ frontend/src/routes/health/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { checkHealth } from "$lib/health";
+  import type { Res } from "$lib/types";
+
+  let health = "unknown";
+
+  onMount(async () => {
+    const result: Res = await checkHealth();
+    health = result.message || "invalid res";
+  });
+</script>
+
+<p>health: {health}</p>
