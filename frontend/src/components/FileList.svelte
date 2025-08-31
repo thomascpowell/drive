ec21fe4ddc2ac7d2c96e193602c98f26ef5ac7fd
Thomas <thomascoxpowell@gmail.com>
2025-08-31 12:49:41 -0400

    feat: add overflow handling

--- frontend/src/components/FileList.svelte
+++ frontend/src/components/FileList.svelte
@@ -14,24 +14,47 @@
     <div><p>del</p></div>
   </div>
 
-  {#each files as file}
-    <div class="line">
-      <div>
-        <FileIcon style="transform: scale(0.6)" />
-        <a href={API_URL + "/files/" + file.ID} download> {file.Filename} </a>
+  <div class="files">
+    {#each files as file}
+      <div class="line">
+        <div>
+          <FileIcon style="transform: scale(0.6)" />
+          <a href={API_URL + "/files/" + file.ID} download> {file.Filename} </a>
+        </div>
+        <div>
+          <p>{file.UploadedAt.substring(5, 10)}</p>
+        </div>
+        <div>
+          <a href="/todo"><Trash style="transform: scale(0.6)" /></a>
+        </div>
       </div>
-      <div>
-        <p>{file.UploadedAt.substring(5, 10)}</p>
-      </div>
-      <div>
-        <a href="/todo"><Trash style="transform: scale(0.6)" /></a>
-      </div>
-    </div>
-  {/each}
+    {/each}
+  </div>
 </div>
 
 <style>
+  .wrapper {
+    border: 0.1em solid var(--border);
+    border-radius: 0.5em;
+    width: 30em;
+  }
+  .wrapper > :first-child {
+    border-radius: 0.4em 0.4em 0 0;
+    background-color: var(--bg3);
+    padding: 1em;
+  }
+  .wrapper :nth-last-child(1) {
+    border-radius: 0 0 0.4em 0.4em !important;
+    border: none !important;
+  }
+
+  .files {
+    max-height: 20em;
+    overflow: scroll;
+  }
+
   .line {
+    width: 100%;
     display: flex;
     gap: 2em;
     border-bottom: 0.1em solid var(--border);
@@ -41,12 +64,11 @@
     display: grid;
     grid-template-columns: 9fr 2fr 1fr;
   }
-
   .line > * {
     display: flex;
     align-items: center;
     text-align: center;
-    min-width: 0;  
+    min-width: 0;
     white-space: nowrap;
   }
   .line > :last-child {
@@ -62,22 +84,8 @@
     display: flex;
     align-items: center;
     text-decoration: none;
-    overflow: hidden!important;
+    overflow: hidden !important;
     max-width: 10em;
     white-space: nowrap;
-  }
-
-  .wrapper {
-    border: 0.1em solid var(--border);
-    border-radius: 0.5em;
-  }
-  .wrapper > :first-child {
-    border-radius: 0.4em 0.4em 0 0;
-    background-color: var(--bg3);
-    padding: 1em;
-  }
-  .wrapper :nth-last-child(1) {
-    border-radius: 0 0 0.4em 0.4em !important;
-    border: none !important;
   }
 </style>
