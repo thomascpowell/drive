aa6db24fd4c342482f0a3f3f88738dbe95c059e1
Thomas <thomascoxpowell@gmail.com>
2025-09-17 10:54:05 -0400

    wip: got redis clone running in docker

---
+++ backend/utils/get_urls.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"os"
+)
+
+// used for cookies
+func GetFrontendURL() string {
+	env := os.Getenv("FE_URL")
+		// env is unset -> no docker -> dev
+		// env is set -> docker -> use the set url
+	if env == "" {
+		return "localhost"
+	}
+	return env
+}
+
+// used for redis (obviously)
+func GetRedisURL() string {
+	env := os.Getenv("ENVIRONMENT")
+		// env is unset -> no docker -> dev
+		// env is set -> docker -> use docker relative
+	if env == "" {
+		return "localhost"
+	}
+	return "redis"
+}
+
