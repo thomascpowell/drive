b35d825f6e09d4297262ea1f74734b4bb21d8dbd
Thomas <thomascoxpowell@gmail.com>
2025-09-22 13:38:56 -0400

    feat: thinner line width for svgs

--- frontend/src/icons/FileIcon.svelte
+++ frontend/src/icons/FileIcon.svelte
@@ -6,10 +6,9 @@
   viewBox="0 0 24 24"
   fill="none"
   stroke="currentColor"
-  stroke-width="2"
+  stroke-width="1.5"
   stroke-linecap="round"
   stroke-linejoin="round"
-  class="icon icon-tabler icons-tabler-outline icon-tabler-file"
   ><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
     d="M14 3v4a1 1 0 0 0 1 1h4"
   /><path
